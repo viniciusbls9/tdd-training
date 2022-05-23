6b36dfd4d1ade4fdedd0673b0ccdfc9901174315
Vinicius Benedito <beneditoloura@gmail.com>
2022-05-23 07:45:09 -0300

    ensure CheckLastEventStatus returns status inReview  when now is after event end time

--- src/tests/check-last-event-status.spec.ts
+++ src/tests/check-last-event-status.spec.ts
@@ -3,8 +3,9 @@ class CheckLastEventStatus {
   constructor(private readonly loadLastEventRepository: LoadLastEventRepository) { }
   async perform({ groupId }: { groupId: string }): Promise<string> {
     const event = await this.loadLastEventRepository.loadLastEvent({ groupId })
-
-    return event === undefined ? 'done' : 'active'
+    if (event === undefined) return 'done'
+    const now = new Date()
+    return event.endDate > now ? 'active' : 'inReview'
   }
 }
 
@@ -73,5 +74,16 @@ describe('CheckLastEventStatus', () => {
     const status = await sut.perform({ groupId })
 
     expect(status).toBe('active')
+  })
+
+  test('Should return status inReview when now is after event end time', async () => {
+    const { sut, loadLastEventRepository } = makeSut()
+    loadLastEventRepository.output = {
+      endDate: new Date(new Date().getTime() - 1)
+    }
+
+    const status = await sut.perform({ groupId })
+
+    expect(status).toBe('inReview')
   })
 })
